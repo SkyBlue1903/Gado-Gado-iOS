ddc4b71903b50f52e871ba9551fd921f48e3c647
Erlangga Anugrah Arifin <angga1433@gmail.com>
2023-08-16 10:37:51 +0700

    Initial Commit

---
+++ Gado Gado/Gado_GadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gado_GadoApp.swift
+//  Gado Gado
+//
+//  Created by Erlangga Anugrah Arifin on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Gado_GadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
